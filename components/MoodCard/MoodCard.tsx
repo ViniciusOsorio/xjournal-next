254ecb87c1a7c0faf7528c3582431e649eefa4f5
LucasMoraes-S <lucas.moruza@gmail.com>
2022-05-13 23:24:54 -0300

    13-05-22

---
+++ components/MoodCard/MoodCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+
+import {
+    OuterWrapper
+} from './MoodCardStyled'
+
+const MoodCard = (props) => {
+
+
+    return(
+        <OuterWrapper></OuterWrapper>
+    )
+}
+
+export default MoodCard;
